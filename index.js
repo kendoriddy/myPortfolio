3d86457586498015574d15a50196c5cd76708200
unknown <okorojiebube2@gmail.com>
2021-07-29 08:08:38 -0600

    added responsiveness

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+// const toggleButton = document.getElementsByClassName('toggle-button')[0]
+// const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
+// toggleButton.addEventListener('click', () => {
+//     navbarLinks.classList.toggle('active')
+// })
+
+const selectElement = function (element) {
+	return document.querySelector('element');
+};
+
+const body = selectElement('body');
+const menuToggler = document.querySelector('.open');
+menuToggler.addEventListener('click', function () {
+	let body = document.querySelector('body');
+	body.classList.toggle('active');
+});
+
+const ScrollToTop = () => {
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth',
+	});
+};
